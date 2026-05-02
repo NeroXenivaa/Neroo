ca367896f156c730380f937bbbe6db9a2ece3ec4
NeroXenivaa <nerooxeniva0101@gmail.com>
2026-05-02 13:00:41 +0700

    p

--- JS/cover.js
+++ JS/cover.js
@@ -4,6 +4,6 @@ const cvrBtn = document.getElementById("cvrBtn");
 
 cvrBtn.addEventListener("click", () => {
     let introWelcome = document.getElementById("introWelcome");
-    introWelcome.classList.remove("w-0", "h-0");
-    introWelcome.classList.add("w-full", "h-screen");
+    introWelcome.classList.remove("h-0");
+    introWelcome.classList.add("h-screen");
 })
