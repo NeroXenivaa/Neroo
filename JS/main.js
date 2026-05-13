cd40f1d8a651fb0d9bb50202c9f80f3590e9e282
NeroXenivaa <nerooxeniva0101@gmail.com>
2026-05-13 19:48:27 +0700

    p

--- JS/main.js
+++ JS/main.js
@@ -32,6 +32,6 @@ window.addEventListener("scroll", () => {
         });
     } else {
         navW.classList.remove("hidden");
-        navW.classList.remove("translate-y-[-25px]", "opacity-[0]");
+        navW.classList.remove("translate-y-[-25px]", "opacity-0");
     }
 });
